a10ed403029fd3ac6a8d4fd7f6594eaedf611f37
pj1963 <2399936456@qq.com>
2020-08-25 01:39:35 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+<script type='text/ecmascript-6'>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang='stylus' rel='stylesheet/stylus'>
+
+</style>
