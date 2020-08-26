3dedc73dd6a213f528919e8b34e360fbcf5f1f56
pj1963 <2399936456@qq.com>
2020-08-26 22:49:26 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import './assets/css/global.css'
 // 配置请求的根路径
 import axios from 'axios'
 axios.defaults.baseURL = 'https://api.naccl.top/vue/shop/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,30 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  breadcrumb,
+  breadcrumbItem,
+  card,
+  row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox
+} from 'element-ui'
 // import { Button } from 'element-ui'
 // import { Form, FormItem } from 'element-ui'
 // import { Input } from 'element-ui'
@@ -7,4 +32,23 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(breadcrumb)
+Vue.use(breadcrumbItem)
+Vue.use(card)
+Vue.use(row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
