a697398e4095b58051f386aec7dc500d67c7944f
pj1963 <2399936456@qq.com>
2020-09-02 01:46:30 +0800

    优化后的项目

--- src/main-dev.js
+++ src/main-dev.js
@@ -15,11 +15,23 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+// 导入 NProgress 包对应的js和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 axios.defaults.baseURL = 'https://api.naccl.top/vue/shop/api/private/v1/'
+
+// 在 request 拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 在最后必须 return config
+  return config
+})
+
+// 在 request 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
